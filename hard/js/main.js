e61a492e7efc93b90044c1a3b14e110abd1377ec
siladigy <sgeorge7731@gmail.com>
2019-07-03 22:36:42 +0300

    hard homework lesson 4

---
+++ hard/js/main.js
@@ -0,0 +1,19 @@
+
+function checkTypeOf(value, callback) {
+    let type = 'не строка';
+    if(typeof(value) == 'string'){
+      type = 'строка';
+    } 
+    console.log(type);
+
+    return callback(value);
+  }
+
+  function checkStrLength(label) {
+      label = label.trim();
+    if(label.length > 30){
+        return label.substr(0, 30) + '...';
+    } else 
+    return label;
+  }
+  console.log(checkTypeOf("   uhwu   ", checkStrLength));
