3584f491b533be18a681cdbd2e0e1076a462749b
siladigy <sgeorge7731@gmail.com>
2019-07-05 12:29:53 +0300

    homework lesson 4 hard fix

--- hard/js/main.js
+++ hard/js/main.js
@@ -1,16 +1,18 @@
-let arr = ["123", "143", "231", "432", "4653", "83", "223"];
-for (i=0; i<arr.length; i++){
-  if(arr[i][0] == 2 || arr[i][0] == 4){
-    console.log(arr[i]);
-  }
+function checkTypeOf(value, callback) {
+  let type = 'не строка';
+  if(typeof(value) == 'string'){
+    type = 'строка';
+  } 
+  console.log(type);
+
+  return callback(value);
 }
 
-for (var i=1;  i<=100; i++) {
-    var count = 0;
-    for (var j=2; j<=i; j++) {
-      if (i%j) {continue;}
-      count += 1;
-    }
-    if (count == 1) console.log(i + " - Делители этого числа: 1 и " + i);
-  }
-  
+function checkStrLength(label) {
+    label = (label.toString()).trim();
+  if(label.length > 30){
+      return label.substr(0, 30) + '...';
+  } else 
+  return label;
+}
+console.log(checkTypeOf("  adasda asd   ", checkStrLength));
