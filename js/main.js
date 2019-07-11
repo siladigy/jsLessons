fc29709bb9c06435964124fe61884e08b0ad4a36
siladigy <sgeorge7731@gmail.com>
2019-07-11 20:09:15 +0300

    practice homework

--- js/main.js
+++ js/main.js
@@ -11,6 +11,15 @@ let money,
     }; 
     start();
 
+function isString(str) {
+    if(isNaN(str)) {
+        return str;
+    } else {
+        str = prompt("Какие обязательные ежемесячные расходы у вас есть?");
+        isString(str);
+    }
+}
+
 let appData = {
     income: {},
     addIncome: [],
@@ -25,7 +34,13 @@ let appData = {
 
         if(confirm("есть ли у вас дополнительный заработок?")) {
             let itemIncome = prompt("какой у вас дополнительный зароботок ?", "таксую");
-            let cashIncome = prompt("Сколько вы на этом зарабатываете в месяц", 9500);
+            while(/^-{0,1}\d+$/.test(itemIncome) || itemIncome == '' || itemIncome == null){
+                itemIncome = prompt("какой у вас дополнительный зароботок ?", "таксую");
+            } 
+            let cashIncome = +prompt("Сколько вы на этом зарабатываете в месяц", 9500);
+            while(isNaN(cashIncome) || cashIncome == '' || cashIncome == null){
+                cashIncome = +prompt("Сколько вы на этом зарабатываете в месяц", 9500);
+            } 
             appData.income[itemIncome] = cashIncome;
         }
 
@@ -35,10 +50,11 @@ let appData = {
         let sum = 0;
             for(let i=0; i<2; i++){
                 if (i===0) {
-                    appData.expenses[prompt("Какие обязательные ежемесячные расходы у вас есть?", "аренда")] = sum = +prompt("Во сколько это обойдется?", 1500);
+                    appData.expenses[isString(prompt("Какие обязательные ежемесячные расходы у вас есть?", "аренда"))] = sum = +prompt("Во сколько это обойдется?", 1500); 
                 } else if (i===1) {
-                    appData.expenses[prompt("Какие обязательные ежемесячные расходы у вас есть?", "продукты")] = sum = +prompt("Во сколько это обойдется?", 850);
-                }     
+                    appData.expenses[isString(prompt("Какие обязательные ежемесячные расходы у вас есть?", "продукты"))] = sum = +prompt("Во сколько это обойдется?", 850); 
+                }  
+                  
                 while(isNaN(sum) || sum == '' || sum == null){
                 sum = +prompt("Во сколько это обойдется?");
                 } 
@@ -76,8 +92,14 @@ let appData = {
     },
     getInfoDeposit: function () {
         if(appData.deposit){
-            appData.percentDeposit = prompt("какой годовой процент?", "10");
-            appData.moneyDeposit =  prompt("какая сумма заложена", 10000);
+            appData.percentDeposit = +prompt("какой годовой процент?", "10");
+            while(isNaN(appData.percentDeposit) || appData.percentDeposit == '' || appData.percentDeposit == null){
+                appData.percentDeposit = +prompt("какой годовой процент?", "10");
+                } 
+            appData.moneyDeposit =  +prompt("какая сумма заложена", 10000);
+            while(isNaN(appData.moneyDeposit) || appData.moneyDeposit == '' || appData.moneyDeposit == null){
+                appData.moneyDeposit =  +prompt("какая сумма заложена", 10000);
+                } 
         }
     },
     calcSavedMoney: function () {
@@ -97,16 +119,15 @@ console.log("Наша программа включает в себя данны
 for (let data in appData){
     console.log(data);
 }
-for (let item in appData.income){
-    console.log("дополнительный источник заработка : " + item);
-    console.log("сумма дополнительного заработка : " + appData.income[item]);
-}
 
-for (let item in appData.expenses){
-    console.log("обязательные расходы : " + item);
-}
-console.log("годовой процент депозита : " + appData.percentDeposit);
-console.log("сумма депозита : " + appData.moneyDeposit);
 
-let str = appData.addExpenses.join();
-console.log(str.toUpperCase());
+let expenses = appData.addExpenses.toString();
+
+function titleCase(str) {
+    let splitStr = str.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+ }
+ console.log(titleCase(expenses));
