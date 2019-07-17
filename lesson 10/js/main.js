0385316acd191a587c7c70db0a807606681d648d
siladigy <sgeorge7731@gmail.com>
2019-07-17 20:10:37 +0300

    lesson 10 homework

---
+++ lesson 10/js/main.js
@@ -0,0 +1,28 @@
+function DomElement(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.createElement = function(){
+        let str;
+        if(selector[0] == "."){
+           str = document.createElement("DIV");
+           str.innerHTML = selector.substr(1);
+           str.setAttribute("class", selector.substr(1));
+           document.body.appendChild(str);
+        } else if(selector[0] == "#"){
+            str = document.createElement("P");
+           str.innerHTML = selector.substr(1);
+           document.body.appendChild(str);
+        }
+        str.style.height = height + "px";
+        str.style.width = width + "px";
+        str.style.background = bg;
+        str.style.fontSize = fontSize + "px";
+    };
+  }
+
+let obj = new DomElement(".hello", 100, 100, "red", 50);
+
+obj.createElement();
