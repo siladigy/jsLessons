1824ebdb7fe8aa389bb0ba3a421a05bcaf7dbfe3
siladigy <sgeorge7731@gmail.com>
2019-07-23 11:13:06 +0300

    fix lesson 12

--- lesson 12/additional/script.js
+++ lesson 12/additional/script.js
@@ -1,31 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(){
 'use strict';
 
-let hours = new Date().getHours(),
+let dayTime = document.querySelector('.dayTime'),
+    weekDay = document.querySelector('.weekDay'),
+    time = document.querySelector('.time'),
+    newYear = document.querySelector('.newYear'),
+    hours = new Date().getHours(),
     day = new Date().getDay(),
-    time = new Date().toLocaleTimeString();
+    currentTime = new Date().toLocaleTimeString();
     
 function getDayTime() {
-    if(hours >= 6 && hours < 12) document.write('Доброе утро');
-    else if(hours >= 12 && hours < 18) document.write('Добрый день');
-    else if(hours >= 18) document.write('Добрый вечер');
-    else if(hours >= 1) document.write('Доброй ночи');
+    if(hours >= 6 && hours < 12) dayTime.textContent = 'Доброе утро';
+    else if(hours >= 12 && hours < 18) dayTime.textContent = 'Добрый день';
+    else if(hours >= 18) dayTime.textContent = 'Добрый вечер';
+    else if(hours >= 1) dayTime.textContent = 'Доброй ночи';
 }
 
 function getWeekDay(){
-    let weekDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];     
-    document.write('<br>Сегодня: ' + weekDay[day - 1]);
+    let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];     
+    weekDay.textContent = 'Сегодня: ' + week[day - 1];
 }
 
 function getTime() {
-    document.write('<br>Текущее время: ' + time);
+    time.textContent = 'Текущее время: ' + currentTime;
 }
 
 function getDaysToNewYear (){
     let ny = new Date('1 jan 2020').getTime(),
         now = new Date().getTime(),
         diff = Math.ceil((ny - now)/1000/60/60/24);
-    document.write('<br>До нового года осталось ' + diff + ' дней');
+        newYear.textContent = 'До нового года осталось ' + diff + ' дней';
 }
 
 getDayTime();
