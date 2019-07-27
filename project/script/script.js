c970048c23a0b6726a5df6349305faed5721295b
siladigy <sgeorge7731@gmail.com>
2019-07-27 17:46:44 +0300

    lesson15

--- project/script/script.js
+++ project/script/script.js
@@ -261,6 +261,40 @@ const slider = () => {
 };
 slider();
 
-
-
 });
+
+// attributes/regulars
+
+const attr = () => {
+
+  const command = document.getElementById('command');
+
+     command.addEventListener('mouseover', (event) => {
+      let target = event.target;
+      if(target.classList.contains('command__photo')){
+        target.dataset.link = target.src;
+        target.src = target.dataset.img;
+      }  
+     });
+
+     command.addEventListener('mouseout', (event) => {
+      let target = event.target;
+      if(target.classList.contains('command__photo')){
+        target.src = target.dataset.link;
+      }  
+     });
+
+
+     const calcBlock = document.querySelector('.calc-block');
+
+     calcBlock.addEventListener('input', (e) => {
+
+      let target = event.target;
+
+      target.value = target.value.replace(/\D/g, '');
+
+     });
+
+}
+attr();
+
